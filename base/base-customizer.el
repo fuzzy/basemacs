0f92913f47cb57698b517960b59ad8b4e96e2590
Mike 'Fuzzy' Partin <fuzzy@thwap.org>
2024-08-10 08:40:08 -0700

    strip everything bare

--- base/base-customizer.el
+++ base/base-customizer.el
@@ -4,19 +4,19 @@
 ;;
 
 ;; define the group
-(defun thwap/generate-set-data (tags-and-modes)
+(defun base/generate-set-data (tags-and-modes)
   "Generate set data for defcustom from TAGS-AND-MODES list."
   (mapconcat (lambda (pair)
                (format "(const :tag \"%s\" %s)" (car pair) (cadr pair)))
              tags-and-modes
              "\n                          "))
 
-(defun thwap/generate-defcustom (custom-var-name custom-var-desc tags-and-modes custom-group)
+(defun base/generate-defcustom (custom-var-name custom-var-desc tags-and-modes custom-group)
   "Generate and evaluate a defcustom block.
 CUSTOM-VAR-NAME is the name of the custom variable.
 TAGS-AND-MODES is a list of pairs where each pair is (tag mode).
 CUSTOM-GROUP is the group for the custom variable."
-  (let* ((set-data (thwap/generate-set-data tags-and-modes))
+  (let* ((set-data (base/generate-set-data tags-and-modes))
         (code (format ";; %s layer toggles
 (defcustom %s nil
   \"%s\"
@@ -31,18 +31,18 @@ CUSTOM-GROUP is the group for the custom variable."
     (eval (read code))))
 
 ;;
-;; Define the custom variables for the T.H.W.A.P. Emacs configuration.
+;; Define the custom variables for the BaseMacs configuration.
 ;;
 
-(defgroup thwap-config nil
-	"T.H.W.A.P. Emacs configuration group."
+(defgroup base-config nil
+	"Emacs configuration group."
   :group 'convenience
-  :prefix "thwap-")
+  :prefix "base-")
 
-(thwap/generate-defcustom 'thwap-layers
+(base/generate-defcustom 'base-layers
 													"Layers of supported features to turn on. (These will create further customization groups)"
-													thwap-layer-toggles
-													'thwap-config)
+													base-layer-toggles
+													'base-config)
 
-(message "T.H.W.A.P. Loaded thwap-config group.")
-(provide 'thwap-customizer)
+(message "BaseMacs: Loaded base-config group.")
+(provide 'base-customizer)
